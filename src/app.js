3b26ce96dc923a390f3827ad734edf3d6e535ab9
priratepotatoe <ninhvh1998@gmail.com>
2024-02-27 10:17:32 +0700

    section 3

--- src/app.js
+++ src/app.js
@@ -1,40 +1,30 @@
 const compression = require('compression');
 const express = require('express');
-const { default: helmet } = require('helmet');
+const helmet = require('helmet'); // Corrected helmet import
 const morgan = require('morgan');
 const app = express();
 
 // Initialize middlewares
-// Example: app.use(middlewareFunction);
-app.use(morgan("dev"))
-//app.use(morgan("combined"))
-app.use(helmet())
-app.use(compression())
-
-
-
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 
 // Initialize database
-
-require('./dbs/init.mongodo')
-const { countConnect } = require('./helpers/check.connect')
-countConnect()
-// Example: databaseConnection();
-
-
-
-
+require('./dbs/init.mongodo'); // Corrected the file name if it was a typo
+const { checkOverload, countConnect } = require('./helpers/check.connect');
+checkOverload();
+countConnect();
 
 // Initialize routers
-// Example: app.use('/api', apiRouter);
-app.get('/', (req, res, next) =>{
-    const strCompress= 'hello daring fox'
+app.get('/', (req, res, next) => {
+    const strCompress = 'hello daring fox';
     return res.status(200).json({
-        message:'Welcome Daring Fox',
+        message: 'Welcome Daring Fox',
         metadata: strCompress.repeat(10000)
-    })
-})
+    });
+});
+
 // Handling errors
 // Example: app.use(errorHandler);
 
-module.exports = app; // Corrected this line
+module.exports = app; // Export the Express app
--- src/dbs/init.mongodo.js
+++ src/dbs/init.mongodo.js
@@ -6,24 +6,26 @@ const connectString = 'mongodb://localhost:27017/demo'
 
 // sử dụng singleton để call 1 lần, thay vì call lại nhiều lần database
 
-class Database{
-    constructor(){
+class Database {
+    constructor() {
         this.connect()
     }
     // sau này nếu có nhiều data khác nhau thì thay vào chỗ mongodb là được
-    connect(type = 'mongodb'){
+    connect(type = 'mongodb') {
 
-        if(1 === 1){
+        if (1 === 1) {
             mongoose.set('debug', true)
-            mongoose.set('debug', {color:true})
+            mongoose.set('debug', { color: true })
         }
 
-        mongoose.connect(connectString).then( _ =>  console.log(`Connected Mongdodb Success`))
-        .catch(err => console.log(`Error Connect!`))
+        mongoose.connect(connectString, {
+            maxPoolSize: 50
+        }).then(_ => console.log(`Connected Mongdodb Success`))
+            .catch(err => console.log(`Error Connect!`))
     }
-   
-    static getInstance(){
-        if(!Database.instance){
+
+    static getInstance() {
+        if (!Database.instance) {
             Database.instance = new Database()
         }
 

24d316d38297a0d60bae60e0ae91cbc8c8ab172c
priratepotatoe <ninhvh1998@gmail.com>
2024-02-28 17:38:26 +0700

    chieu 28th Section 5

--- src/models/keytoken.model.js
+++ src/models/keytoken.model.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const { Schema, model } = require("mongoose");
+
+const DOCUMENT_NAME = 'Key';
+const COLLECTION_NAME = 'Keys';
+
+var keyTokenSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        unique: true,
+        index: true,
+        ref: 'Shop'
+    },
+    publicKey: {
+        type: String,
+        required: true
+    }
+    , refreshToken: {
+        type: Array,
+        default: []
+    }}
+    ,{
+        collection: COLLECTION_NAME,
+        timestamps: true
+    
+})
+
+module.exports = model(DOCUMENT_NAME, keyTokenSchema);
---
+++ src/services/keyToken.service.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class KeyTokenService{
+    static createKeyToke = async ({user, publicKey}) =>{
+        try {
+            const publicKeyString = publicKey.toString();
+        } catch (error) {
+            
+        }
+    }
+}
+module.exports = KeyTokenService;

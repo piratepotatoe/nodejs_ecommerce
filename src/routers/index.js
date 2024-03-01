244a0c4095f2837feeea0d28d0d7eb41083677a1
priratepotatoe <ninhvh1998@gmail.com>
2024-03-01 17:35:28 +0700

    index api model

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,11 +5,5 @@ const router = express.Router()
 
 router.use('/v1/api', require('./access'))
 
-// router.get('', (req, res, next) => {
-//     const strCompress = 'hello daring fox';
-//     return res.status(200).json({
-//         message: 'Welcome Daring Fox',
-//         metadata: strCompress.repeat(10000)
-//     });
-// });
+
 module.exports = router

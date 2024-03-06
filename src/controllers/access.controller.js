5c459c5a22f7da8a9645f4ca87df89618c213de3
priratepotatoe <ninhvh1998@gmail.com>
2024-03-06 10:08:02 +0700

    Section 8 xử lý success response khi sign up thành công

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,18 +1,17 @@
 'use strict';
 const AccessService = require("../services/access.service")
 
+const{OK, CREATED} = require("../core/success.response")
+
 class AccessController {
     signUp = async (req, res, next) => {
-
-        /*
-        200 OK
-        201 Created
-        */
-
-        return res.status(201).json(
-            await AccessService.signUp(req.body)
-        )
-
+        new CREATED({
+            message:'Registration successfully',
+            metadata: await AccessService.signUp(req.body),
+            options:{
+                limit:10
+            }
+        }).send(res)
     }
 }
 

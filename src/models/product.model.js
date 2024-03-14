33ddd746d8e53b422f36ca1a5991d25073980ed7
priratepotatoe <ninhvh1998@gmail.com>
2024-03-14 10:07:46 +0700

    Complete Section 12, giới thiệu sơ qua về product và cách quản lý nhiều product

---
+++ src/models/product.model.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const { model, Schema } = require('mongoose'); // Erase if already required
+const DOCUMENT_NAME = 'Product';
+const COLLECTION_NAME = 'Products'
+
+const productSchema = new Schema({
+    product_name: { type: String, required: true },
+    product_thumb: { type: String, required: true },
+    product_description: String,
+    product_price: { type: Number, required: true },
+    product_quantity: { type: Number, required: true },
+    product_type: { type: String, required: true, enum: ['Electronic', 'Clothing', 'Furniture'] },
+    product_shop: {type: Schema.Types.ObjectId, ref: 'Shop'},
+    product_attribute: { type: Schema.Types.Mixed, required: true }
+},
+    {
+        collection: COLLECTION_NAME,
+        timestamps: true
+    }
+)
+
+
+//define the product type
+const clothingSchema = new Schema({
+    brand: { type: String, required: true },
+    size: String, //Section 11: fix 1 size trước, các section sau sẽ update nhiều size
+    material: String
+
+}, {
+    collection: 'clothes',
+    timestamps: true
+})
+
+
+//define the product type = Electronics product
+const electronicSchema = new Schema({
+    manufacturer: { type: String, required: true },
+    model: String, //Section 11: fix 1 size trước, các section sau sẽ update nhiều size
+    color: String
+
+}, {
+    collection: 'electronics',
+    timestamps: true
+})
+
+module.exports = {
+    product:model(DOCUMENT_NAME,productSchema),
+    clothing:model('Clothes',clothingSchema),
+    electronic:model('Electronics',electronicSchema),
+}
